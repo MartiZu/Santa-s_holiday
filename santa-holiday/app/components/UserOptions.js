7efea1bbb44e821c778adae89cbf22e25c68b38f
MartiZu <martina.zurli@gmail.com>
2024-01-20 13:36:04 +0000

    drop down

--- santa-holiday/app/components/UserOptions.js
+++ santa-holiday/app/components/UserOptions.js
@@ -14,31 +14,37 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export default function UserOptions({ trips }) {
+  console.log("@@@@@@@@@@@@@@@@@@@@@@@@@", trips);
   const [selectedTravel, setSelectedTravel] = useState(null);
+  const [selection, setSelection] = useState(null);
 
   const handleTravelSelection = (travel) => {
     setSelectedTravel(travel);
+    setSelection(travel);
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline">Choose your travel destination</Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>Travel Options</DropdownMenuLabel>
-        <DropdownMenuSeparator />
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Choose your travel destination</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>Travel Options</DropdownMenuLabel>
+          <DropdownMenuSeparator />
 
-        {trips.map((travel) => (
-          <DropdownMenuItem
-            key={travel.id}
-            checked={selectedTravel === travel}
-            onCheckedChange={() => handleTravelSelection(travel)}
-          >
-            {`${travel.start} to ${travel.end}`}
-          </DropdownMenuItem>
-        ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+          {trips.map((travel) => (
+            <DropdownMenuItem
+              key={travel.id}
+              checked={selectedTravel}
+              onCheckedChange={() => handleTravelSelection(travel)}
+            >
+              {`${travel.start} to ${travel.end}`}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <div>Hello {selection}</div>
+    </>
   );
 }
